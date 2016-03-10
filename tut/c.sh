fee27411dd313e0aca192b2eae9adcc351076c01
James McMurray <jamesmcm03@gmail.com>
2016-03-10 20:56:22 +0000

    First demo in libtcod

---
+++ tut/c.sh
@@ -0,0 +1,1 @@
+g++ ./*.cpp -I/usr/include/libtcod -ltcod -ltcodxx -o test.out
---
+++ tut/main.cpp
@@ -0,0 +1,28 @@
+#include "libtcod.hpp"
+int main() {
+  int playerx=40,playery=25;
+  TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
+  while ( !TCODConsole::isWindowClosed() ) {
+     TCOD_key_t key;
+     TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+     switch(key.vk) {
+        case TCODK_UP : playery--; break;
+        case TCODK_DOWN : playery++; break;
+        case TCODK_LEFT : playerx--; break;
+        case TCODK_RIGHT : playerx++; break;
+        case TCODK_CHAR : switch(key.c){ 
+          case 'h': playerx--; break;
+          case 'j': playery++; break;
+          case 'k': playery--; break;
+          case 'l': playerx++; break;
+          default: break;
+        }
+        default: break;
+     }
+     TCODConsole::root->clear();
+     TCODConsole::root->putChar(playerx,playery,'@');
+     TCODConsole::flush();
+  }
+  return 0;
+}
+
